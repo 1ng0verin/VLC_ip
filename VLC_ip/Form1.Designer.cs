4f0fa49f07b36f6d5284d94a7b9f2d1b4ca66c7d
1ng0verin <52739822+1ng0verin@users.noreply.github.com>
2019-07-31 17:27:51 +0500

    Add files via upload

---
+++ VLC_ip/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace VLC_ip
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.textBoxStrCnt = new System.Windows.Forms.TextBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            this.vlcControl2 = new Vlc.DotNet.Forms.VlcControl();
+            this.textBoxStrCnt2 = new System.Windows.Forms.TextBox();
+            this.buttonPlay2 = new System.Windows.Forms.Button();
+            this.textBoxStrCnt3 = new System.Windows.Forms.TextBox();
+            this.buttonPlay3 = new System.Windows.Forms.Button();
+            this.vlcControl3 = new Vlc.DotNet.Forms.VlcControl();
+            this.textBoxStrCnt4 = new System.Windows.Forms.TextBox();
+            this.textBoxStrCnt5 = new System.Windows.Forms.TextBox();
+            this.buttonPlay4 = new System.Windows.Forms.Button();
+            this.textBoxStrCnt6 = new System.Windows.Forms.TextBox();
+            this.buttonPlay5 = new System.Windows.Forms.Button();
+            this.vlcControl4 = new Vlc.DotNet.Forms.VlcControl();
+            this.buttonPlay6 = new System.Windows.Forms.Button();
+            this.vlcControl5 = new Vlc.DotNet.Forms.VlcControl();
+            this.vlcControl6 = new Vlc.DotNet.Forms.VlcControl();
+            this.SuspendLayout();
+            // 
+            // textBoxStrCnt
+            // 
+            this.textBoxStrCnt.Location = new System.Drawing.Point(6, 423);
+            this.textBoxStrCnt.Name = "textBoxStrCnt";
+            this.textBoxStrCnt.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt.TabIndex = 0;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(467, 422);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "PLAY";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Location = new System.Drawing.Point(6, 12);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Rate = 0F;
+            this.vlcControl1.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl1.TabIndex = 2;
+            this.vlcControl1.Text = "vlcControl1";
+            // 
+            // vlcControl2
+            // 
+            this.vlcControl2.BackColor = System.Drawing.Color.Black;
+            this.vlcControl2.Location = new System.Drawing.Point(548, 12);
+            this.vlcControl2.Name = "vlcControl2";
+            this.vlcControl2.Rate = 0F;
+            this.vlcControl2.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl2.TabIndex = 2;
+            this.vlcControl2.Text = "vlcControl1";
+            // 
+            // textBoxStrCnt2
+            // 
+            this.textBoxStrCnt2.Location = new System.Drawing.Point(548, 425);
+            this.textBoxStrCnt2.Name = "textBoxStrCnt2";
+            this.textBoxStrCnt2.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt2.TabIndex = 0;
+            // 
+            // buttonPlay2
+            // 
+            this.buttonPlay2.Location = new System.Drawing.Point(1009, 424);
+            this.buttonPlay2.Name = "buttonPlay2";
+            this.buttonPlay2.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay2.TabIndex = 1;
+            this.buttonPlay2.Text = "PLAY";
+            this.buttonPlay2.UseVisualStyleBackColor = true;
+            this.buttonPlay2.Click += new System.EventHandler(this.buttonPlay2_Click);
+            // 
+            // textBoxStrCnt3
+            // 
+            this.textBoxStrCnt3.Location = new System.Drawing.Point(1090, 425);
+            this.textBoxStrCnt3.Name = "textBoxStrCnt3";
+            this.textBoxStrCnt3.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt3.TabIndex = 0;
+            // 
+            // buttonPlay3
+            // 
+            this.buttonPlay3.Location = new System.Drawing.Point(1551, 424);
+            this.buttonPlay3.Name = "buttonPlay3";
+            this.buttonPlay3.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay3.TabIndex = 1;
+            this.buttonPlay3.Text = "PLAY";
+            this.buttonPlay3.UseVisualStyleBackColor = true;
+            this.buttonPlay3.Click += new System.EventHandler(this.buttonPlay3_Click);
+            // 
+            // vlcControl3
+            // 
+            this.vlcControl3.BackColor = System.Drawing.Color.Black;
+            this.vlcControl3.Location = new System.Drawing.Point(1090, 12);
+            this.vlcControl3.Name = "vlcControl3";
+            this.vlcControl3.Rate = 0F;
+            this.vlcControl3.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl3.TabIndex = 2;
+            this.vlcControl3.Text = "vlcControl1";
+            // 
+            // textBoxStrCnt4
+            // 
+            this.textBoxStrCnt4.Location = new System.Drawing.Point(6, 862);
+            this.textBoxStrCnt4.Name = "textBoxStrCnt4";
+            this.textBoxStrCnt4.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt4.TabIndex = 0;
+            // 
+            // textBoxStrCnt5
+            // 
+            this.textBoxStrCnt5.Location = new System.Drawing.Point(548, 864);
+            this.textBoxStrCnt5.Name = "textBoxStrCnt5";
+            this.textBoxStrCnt5.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt5.TabIndex = 0;
+            // 
+            // buttonPlay4
+            // 
+            this.buttonPlay4.Location = new System.Drawing.Point(467, 861);
+            this.buttonPlay4.Name = "buttonPlay4";
+            this.buttonPlay4.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay4.TabIndex = 1;
+            this.buttonPlay4.Text = "PLAY";
+            this.buttonPlay4.UseVisualStyleBackColor = true;
+            this.buttonPlay4.Click += new System.EventHandler(this.buttonPlay4_Click);
+            // 
+            // textBoxStrCnt6
+            // 
+            this.textBoxStrCnt6.Location = new System.Drawing.Point(1090, 864);
+            this.textBoxStrCnt6.Name = "textBoxStrCnt6";
+            this.textBoxStrCnt6.Size = new System.Drawing.Size(455, 20);
+            this.textBoxStrCnt6.TabIndex = 0;
+            // 
+            // buttonPlay5
+            // 
+            this.buttonPlay5.Location = new System.Drawing.Point(1009, 863);
+            this.buttonPlay5.Name = "buttonPlay5";
+            this.buttonPlay5.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay5.TabIndex = 1;
+            this.buttonPlay5.Text = "PLAY";
+            this.buttonPlay5.UseVisualStyleBackColor = true;
+            this.buttonPlay5.Click += new System.EventHandler(this.buttonPlay5_Click);
+            // 
+            // vlcControl4
+            // 
+            this.vlcControl4.BackColor = System.Drawing.Color.Black;
+            this.vlcControl4.Location = new System.Drawing.Point(6, 451);
+            this.vlcControl4.Name = "vlcControl4";
+            this.vlcControl4.Rate = 0F;
+            this.vlcControl4.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl4.TabIndex = 2;
+            this.vlcControl4.Text = "vlcControl1";
+            // 
+            // buttonPlay6
+            // 
+            this.buttonPlay6.Location = new System.Drawing.Point(1551, 863);
+            this.buttonPlay6.Name = "buttonPlay6";
+            this.buttonPlay6.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay6.TabIndex = 1;
+            this.buttonPlay6.Text = "PLAY";
+            this.buttonPlay6.UseVisualStyleBackColor = true;
+            this.buttonPlay6.Click += new System.EventHandler(this.buttonPlay6_Click);
+            // 
+            // vlcControl5
+            // 
+            this.vlcControl5.BackColor = System.Drawing.Color.Black;
+            this.vlcControl5.Location = new System.Drawing.Point(548, 451);
+            this.vlcControl5.Name = "vlcControl5";
+            this.vlcControl5.Rate = 0F;
+            this.vlcControl5.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl5.TabIndex = 2;
+            this.vlcControl5.Text = "vlcControl1";
+            // 
+            // vlcControl6
+            // 
+            this.vlcControl6.BackColor = System.Drawing.Color.Black;
+            this.vlcControl6.Location = new System.Drawing.Point(1090, 451);
+            this.vlcControl6.Name = "vlcControl6";
+            this.vlcControl6.Rate = 0F;
+            this.vlcControl6.Size = new System.Drawing.Size(536, 403);
+            this.vlcControl6.TabIndex = 2;
+            this.vlcControl6.Text = "vlcControl1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1639, 909);
+            this.Controls.Add(this.vlcControl6);
+            this.Controls.Add(this.vlcControl3);
+            this.Controls.Add(this.vlcControl5);
+            this.Controls.Add(this.vlcControl2);
+            this.Controls.Add(this.buttonPlay6);
+            this.Controls.Add(this.buttonPlay3);
+            this.Controls.Add(this.vlcControl4);
+            this.Controls.Add(this.buttonPlay5);
+            this.Controls.Add(this.vlcControl1);
+            this.Controls.Add(this.textBoxStrCnt6);
+            this.Controls.Add(this.buttonPlay2);
+            this.Controls.Add(this.buttonPlay4);
+            this.Controls.Add(this.textBoxStrCnt3);
+            this.Controls.Add(this.textBoxStrCnt5);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.textBoxStrCnt4);
+            this.Controls.Add(this.textBoxStrCnt2);
+            this.Controls.Add(this.textBoxStrCnt);
+            this.Name = "MainForm";
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxStrCnt;
+		private System.Windows.Forms.Button buttonPlay;
+		private Vlc.DotNet.Forms.VlcControl vlcControl1;
+        private Vlc.DotNet.Forms.VlcControl vlcControl2;
+        private System.Windows.Forms.TextBox textBoxStrCnt2;
+        private System.Windows.Forms.Button buttonPlay2;
+        private System.Windows.Forms.TextBox textBoxStrCnt3;
+        private System.Windows.Forms.Button buttonPlay3;
+        private Vlc.DotNet.Forms.VlcControl vlcControl3;
+        private System.Windows.Forms.TextBox textBoxStrCnt4;
+        private System.Windows.Forms.TextBox textBoxStrCnt5;
+        private System.Windows.Forms.Button buttonPlay4;
+        private System.Windows.Forms.TextBox textBoxStrCnt6;
+        private System.Windows.Forms.Button buttonPlay5;
+        private Vlc.DotNet.Forms.VlcControl vlcControl4;
+        private System.Windows.Forms.Button buttonPlay6;
+        private Vlc.DotNet.Forms.VlcControl vlcControl5;
+        private Vlc.DotNet.Forms.VlcControl vlcControl6;
+    }
+}
+
---
+++ VLC_ip/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLC_ip
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonPlay_Click(object sender, EventArgs e)
+		{
+           
+            
+            string path = textBoxStrCnt.Text;
+            var mediaOptions = new[]
+                {
+                    ":sout=#rtp{sdp=rtsp://127.0.0.1:554/}",
+                    ":sout-keep"
+                };
+            vlcControl1.SetMedia(new Uri("http://hls1.addictradio.net/addictrock_aac_hls/playlist.m3u8"),
+                    mediaOptions);
+            vlcControl1.Play();
+            
+
+        }
+
+        private void buttonPlay2_Click(object sender, EventArgs e)
+        {
+            //string path = textBoxStrCnt2.Text;
+            //Vlc.DotNet.Core.Medias.LocationMedia media = new Vlc.DotNet.Core.Medias.LocationMedia(path);
+            //vlcControl2.Media = media;
+            //vlcControl2.Play();
+        }
+
+        private void buttonPlay3_Click(object sender, EventArgs e)
+        {
+            //string path = textBoxStrCnt3.Text;
+            //Vlc.DotNet.Core.Medias.LocationMedia media = new Vlc.DotNet.Core.Medias.LocationMedia(path);
+            //vlcControl3.Media = media;
+            //vlcControl3.Play();
+        }
+
+        private void buttonPlay4_Click(object sender, EventArgs e)
+        {
+            //string path = textBoxStrCnt4.Text;
+            //Vlc.DotNet.Core.Medias.LocationMedia media = new Vlc.DotNet.Core.Medias.LocationMedia(path);
+            //vlcControl4.Media = media;
+            //vlcControl4.Play();
+        }
+
+        private void buttonPlay5_Click(object sender, EventArgs e)
+        {
+            //string path = textBoxStrCnt5.Text;
+            //Vlc.DotNet.Core.Medias.LocationMedia media = new Vlc.DotNet.Core.Medias.LocationMedia(path);
+            //vlcControl5.Media = media;
+            //vlcControl5.Play();
+        }
+
+        private void buttonPlay6_Click(object sender, EventArgs e)
+        {
+            //string path = textBoxStrCnt6.Text;
+            //Vlc.DotNet.Core.Medias.LocationMedia media = new Vlc.DotNet.Core.Medias.LocationMedia(path);
+            //vlcControl6.Media = media;
+            //vlcControl6.Play();
+        }
+    }
+}
